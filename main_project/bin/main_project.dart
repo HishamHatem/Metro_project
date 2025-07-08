4ce85969cae49b5496787142ab5fe89a0723a2f6
HishamHatem <heshamhatem08@gmail.com>
2025-07-08 11:42:15 +0300

    Add initial project structure with configuration files and basic functionality

---
+++ main_project/bin/main_project.dart
@@ -0,0 +1,11 @@
+/*
+we will make a simple dart project that will be like a metro application
+it will ask user for start station and end station
+and then it will show the shortest path between them depending on number of stations
+
+first we will declare the 3 lines of metro
+second we will ask the user for start and end stations
+then we will find the shortest path between them
+we will use BFS algorithm to find the shortest path
+we will use a map to represent the metro lines and stations
+*/
---
+++ main_project/test/main_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:main_project/main_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
